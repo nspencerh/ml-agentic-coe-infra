e16ba39e31634dfa8e8ab8c2c224f41a9a48d07b
nspencer-transurban <nspencer@transurban.com>
2026-05-14 12:03:25 +1000

    refactor(gemini-enterprise-core): comment module discovery_engine for the moment

--- component/gemini-enterprise-core/discoveryengine.tf
+++ component/gemini-enterprise-core/discoveryengine.tf
@@ -1,14 +1,14 @@
-module "discovery_engine" {
-  source = "../../modules/discovery-engine"
+# module "discovery_engine" {
+#   source = "../../modules/discovery-engine"
 
-  project_id            = var.project_id
-  engine_id             = var.ephemeral_suffix != null ? "${var.gemini_app.engine_id}-${var.ephemeral_suffix}" : var.gemini_app.engine_id
-  display_name          = var.ephemeral_suffix != null ? "${var.gemini_app.display_name} (${var.ephemeral_suffix})" : var.gemini_app.display_name
-  location              = var.gemini_app.location
-  collection_id         = var.gemini_app.collection_id
-  data_store_ids        = var.gemini_app.data_store_ids
-  industry_vertical     = var.gemini_app.industry_vertical
-  default_language_code = var.gemini_app.default_language_code
-  time_zone             = var.gemini_app.time_zone
-  company_name          = var.gemini_app.company_name
-}
+#   project_id            = var.project_id
+#   engine_id             = var.ephemeral_suffix != null ? "${var.gemini_app.engine_id}-${var.ephemeral_suffix}" : var.gemini_app.engine_id
+#   display_name          = var.ephemeral_suffix != null ? "${var.gemini_app.display_name} (${var.ephemeral_suffix})" : var.gemini_app.display_name
+#   location              = var.gemini_app.location
+#   collection_id         = var.gemini_app.collection_id
+#   data_store_ids        = var.gemini_app.data_store_ids
+#   industry_vertical     = var.gemini_app.industry_vertical
+#   default_language_code = var.gemini_app.default_language_code
+#   time_zone             = var.gemini_app.time_zone
+#   company_name          = var.gemini_app.company_name
+# }
