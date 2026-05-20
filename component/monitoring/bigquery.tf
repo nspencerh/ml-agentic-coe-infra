339ad9df42f0593f18d9d9a3ea18a8d1f6d83ae4
nspencer-transurban <nspencer@transurban.com>
2026-05-20 22:57:34 +1000

    [AIOPS-104] test cicd

--- component/monitoring/bigquery.tf
+++ component/monitoring/bigquery.tf
@@ -9,7 +9,7 @@ resource "google_bigquery_dataset" "deployments" {
     managed_by  = "terraform"
   }
 }
-###
+
 # resource "google_bigquery_table" "deployment_events" {
 #   dataset_id          = google_bigquery_dataset.deployments.dataset_id
 #   table_id            = "deployment_events"
